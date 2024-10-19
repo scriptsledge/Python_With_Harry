31fa85833c17f068f1e9033deaab280a3de88bed
Script Sledge <scriptsledge@users.noreply.github.com>
2024-10-19 09:41:29 +0530

    Day 4.1

--- Chapter_3/01_Strings.py
+++ Chapter_3/01_Strings.py
@@ -1,18 +1,23 @@
-from torch import slice_scatter
+# String examples
+a = 'a'          # This is a string
+a = "apple"      # This is also a string
+a = '''badaapple'''  # This is also a string
 
+# Strings are immutable
 
-a = 'a' # this is a string
-a = "apple" # this is also a string
-a = '''bada
-apple''' # this is also a string
-# strings are immutable
-
-# slicing
+# Slicing example
 name = 'script'
-print(len(name)) # we can know length of string using len function
-sliced_string = name[2: 6] # start from index 2 all the way till 6 (excluding 6)
-print(sliced_string)
-print(name[1]) # to fetch a character
 
-# indices starts from 0 (left to right)
-# indices starts from -1 (right to left)
+# Get the length of the string
+print(len(name))  # Output: 6
+
+# Slicing the string
+sliced_string = name[2:6]  # Start from index 2 up to (but not including) index 6
+print(sliced_string)       # Output: 'ript'
+
+# Fetching a character by index
+print(name[1])  # Output: 'c'
+
+# Note:
+# Indices start from 0 (left to right)
+# Indices start from -1 (right to left)
