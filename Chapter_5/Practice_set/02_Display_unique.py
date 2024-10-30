540055eb272741ae710339ee1600d9575e2e5856
Script Sledge <scriptsledge@users.noreply.github.com>
2024-10-30 19:41:17 +0530

    Day 15

---
+++ Chapter_5/Practice_set/02_Display_unique.py
@@ -0,0 +1,30 @@
+# Set to store unique numbers entered by the user
+uniSet = set()
+
+# Adding each entered number to the set; duplicates are automatically ignored
+a = int(input("Enter the number 1:"))
+uniSet.add(a)
+
+a = int(input("Enter the number 2:"))
+uniSet.add(a)
+
+a = int(input("Enter the number 3:"))
+uniSet.add(a)
+
+a = int(input("Enter the number 4:"))
+uniSet.add(a)
+
+a = int(input("Enter the number 5:"))
+uniSet.add(a)
+
+a = int(input("Enter the number 6:"))
+uniSet.add(a)
+
+a = int(input("Enter the number 7:"))
+uniSet.add(a)
+
+a = int(input("Enter the number 8:"))
+uniSet.add(a)
+
+# Print the set of unique numbers
+print(uniSet)
---
+++ Chapter_5/Practice_set/03_18_in_set.py
@@ -0,0 +1,8 @@
+# Set containing both integer and string representations of 18
+vals = {18, "18"}
+
+# Demonstrates that sets can hold different data types, even if values look similar
+print(vals)  # Output: {18, '18'}
+
+# Explanation:
+# Since 18 (integer) and "18" (string) are different data types, they are treated as unique elements in the set.
