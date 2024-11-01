a134355aff00f5704ff834080cc38c487fded877
Script Sledge <scriptsledge@users.noreply.github.com>
2024-11-01 18:33:56 +0530

    Day 17.1

--- Chapter_5/05_Set_union_intersection.py
+++ Chapter_5/05_Set_union_intersection.py
@@ -1,5 +1,8 @@
 s1 = {1, 45, 6, 78}
 s2 = {7, 8, 1, 78}
 
-print(s1.union(s2))
-print(s1.intersection(s2))
+# union(): combines elements from both sets without duplicates
+print(s1.union(s2))  # Output: {1, 6, 7, 8, 45, 78}
+
+# intersection(): finds elements that are common in both sets
+print(s1.intersection(s2))  # Output: {1, 78}
