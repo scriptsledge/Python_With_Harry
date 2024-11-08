c9c16235c0a17aedb604348bbf79370d686260b8
Script Sledge <scriptsledge@users.noreply.github.com>
2024-11-08 17:40:18 +0530

    Day 23.3

---
+++ Chapter_6/Practice_set/06_Student_grade.py
@@ -0,0 +1,13 @@
+marks = int(input("Enter your marks:"))
+if (marks<50):
+    print("Your grade is: F")
+elif (marks<60):
+    print("Your grade is: D")
+elif (marks<70):
+    print("Your grade is: C")
+elif (marks<80):
+    print("Your grade is: B")
+elif (marks<90):
+    print("Your grade is: A")
+else :
+    print("Your grade is: Ex")
