8cb8cddaecf10fd564a2d5731e5c8b73133ca212
Script Sledge <scriptsledge@users.noreply.github.com>
2024-11-08 17:48:57 +0530

    Day 23.4.1

--- Chapter_6/Practice_set/07_If_Harry_in_post.py
+++ Chapter_6/Practice_set/07_If_Harry_in_post.py
@@ -1,7 +1,7 @@
 post = input("Enter the post heading:")
 
-# Check if "Harry" (case-insensitive) is mentioned in the post
-if "Harry" in post or "harry" in post:
+# Convert the post to lowercase and check if "harry" is mentioned
+if "harry" in post.lower():
     print("The post is talking about Harry.")
 else:
     print("The post is not talking about Harry.")
