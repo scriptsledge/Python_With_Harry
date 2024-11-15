8af0ea7753aa4f29cf6e284a091a5337a4877068
Script Sledge <scriptsledge@users.noreply.github.com>
2024-11-15 23:22:33 +0530

    Day 30

--- Chapter_9/01_File.py
+++ Chapter_9/01_File.py
@@ -1,4 +1,10 @@
-f = open("Chapter_9/file.txt")
+# Reading a file using Python
+# Open a file in read mode ('r')
+f = open("Chapter_9/file.txt", "r")
+
+# Read the content of the file
 data = f.read()
 print(data)
+
+# Close the file to free resources
 f.close()
